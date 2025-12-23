dae863cc8ab41c4696ca1c44458b9f7154d47dcb
Michael Honaker <mchonaker@gmail.com>
2025-12-22 22:31:08 -0500

    Update types

--- project-mapper-core/src/types/openapi.rs
+++ project-mapper-core/src/types/openapi.rs
@@ -28,3 +28,35 @@ impl Default for OpenAPISchema {
         }))
     }
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct OpenAPI(serde_json::Value);
+
+impl OpenAPI {
+    pub fn to_json_value(&self) -> serde_json::Value {
+        self.0.clone()
+    }
+}
+impl TryFrom<serde_json::Value> for OpenAPI {
+    type Error = String;
+    fn try_from(value: serde_json::Value) -> Result<Self, Self::Error> {
+        if value.get("openapi") == None {
+            return Err("Invalid OpenAPI format: missing 'openapi' field missing".to_string());
+        }
+        if value.get("info") == None {
+            return Err("Invalid OpenAPI format: missing 'info' field missing".to_string());
+        }
+        Ok(OpenAPI(value))
+    }
+}
+
+impl Default for OpenAPI {
+    fn default() -> Self {
+        OpenAPI(serde_json::json!({
+            "opnapi": "3.1.0",
+            "info": {
+                "title":"ProjectMapper"
+            },
+        }))
+    }
+}
